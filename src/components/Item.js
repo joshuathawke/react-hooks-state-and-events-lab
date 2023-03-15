78d8d4a091b8f3e34d53639b5f2ab73a91776ac5
Josh Haawke <joshuathawke@gmail.com>
2023-03-15 14:41:29 -0400

    changes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, toggleCart] = useState() 
+  function handleClick()  {
+      toggleCart(!inCart);
+  }
+  
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? 'remove' : 'add'} onClick={handleClick}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+
+
+// If the item is not in the cart, the <button> element's 
+// text should read "Add to Cart", and if the item is in the cart,
+//  the <button> element's text should read "Remove From Cart". 
+//  Naturally, you'll also need to add state to the Item component 
+//  to solve this deliverable!
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selected, changeSelected] = useState("All")
+  function handleChange(e) {
+    changeSelected(e.target.value)
+  }
+
+  const itemsToShow = items.filter( item => {
+    if (selected === "All")
+    return true
+    else 
+    return (item.category === selected)
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToShow.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
